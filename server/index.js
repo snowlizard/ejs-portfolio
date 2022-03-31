727bc2c6e7668fb71d42272b6b34e73a3aac6802
julian <illegal_burrito@live.com>
2022-03-31 08:56:27 -0700

    fixed node typos

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
-const PORT  = process.env.port || 8080;
+const PORT  = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
     console.log(`listing on port ${PORT}`)
